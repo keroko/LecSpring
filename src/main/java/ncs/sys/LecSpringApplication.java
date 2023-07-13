62c25c1a645adfc537d94fcd0f61d6ea3611c585
keroko <keroko.ct@gmail.com>
2023-07-13 17:46:59 +0900

    20230713

---
+++ src/main/java/ncs/sys/LecSpringApplication.java
@@ -0,0 +1,13 @@
+package ncs.sys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LecSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LecSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ncs/sys/LecSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package ncs.sys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LecSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
